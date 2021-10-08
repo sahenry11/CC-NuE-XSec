c8986cf2ee338cd9a3818ed875966e554a3c348e
Sarah Henry <shenry@minervagpvm03.fnal.gov>
2021-10-08 14:22:02 -0500

    nuebar config source

---
+++ setup_ccantinue.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export CCNUEROOT=$DIR
+export PYTHONPATH=$CCNUEROOT:$PYTHONPATH
+# Alternative configuration folder
+if [ -z "$1" ]; then
+config="CCAntiNuE"
+else 
+config=$1
+fi
+echo $config
+export CCNuEConfig=$config
+export PYTHONPATH=${CCNUEROOT}/configs/${config}:$PYTHONPATH
